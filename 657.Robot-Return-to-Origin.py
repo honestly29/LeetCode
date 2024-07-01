4d199ba92472f7d78e9d4e3670f8c5d6fda24dec
honestly29 <137715056+honestly29@users.noreply.github.com>
2024-07-01 21:45:23 +0100

    Add files via upload

---
+++ 657.Robot-Return-to-Origin.py
@@ -0,0 +1,9 @@
+class Solution:
+    def judgeCircle(self, moves: str) -> bool:
+        
+        
+        if moves.count("U") == moves.count("D") and moves.count("R") == moves.count("L"):
+            return True
+        
+        return False
+
