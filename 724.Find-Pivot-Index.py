5187c754797113d08fe6f9dbcc914d66c336d856
honestly29 <137715056+honestly29@users.noreply.github.com>
2024-10-05 19:58:12 +0100

    Add files via upload

---
+++ 724.Find-Pivot-Index.py
@@ -0,0 +1,17 @@
+class Solution:
+    def pivotIndex(self, nums: List[int]) -> int:
+        
+        sumLeft = 0
+        sumRight = sum(nums)
+
+        for idx, ele in enumerate(nums):
+
+            sumRight -= ele
+
+            if sumLeft == sumRight:
+
+                return idx
+
+            sumLeft += ele
+
+        return -1
